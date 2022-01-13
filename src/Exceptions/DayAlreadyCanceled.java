2dcc5d801980594dad0d5a068edc9cc98ee1febc
Anaa10 <a93284@alunos.uminho.pt>
2022-01-13 01:02:22 +0000

    ...

---
+++ src/Exceptions/DayAlreadyCanceled.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class DayAlreadyCanceled extends Exception{
+    
+    public DayAlreadyCanceled(String e){
+        super(e);
+    }
+}
---
+++ src/Exceptions/ReservationAlreadyMade.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class ReservationAlreadyMade {
+    
+    public ReservationAlreadyMade(String e){
+        super(e);
+    }
+}
